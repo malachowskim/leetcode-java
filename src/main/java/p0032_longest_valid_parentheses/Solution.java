5b6ecbefa06fb8a9a0640956a4dcdfc51a7f5218
malachowskim <160923270+malachowskim@users.noreply.github.com>
2026-07-15 12:01:14 +0200

    Task improved 32 solution

--- src/main/java/p0032_longest_valid_parentheses/Solution.java
+++ src/main/java/p0032_longest_valid_parentheses/Solution.java
@@ -1,8 +1,5 @@
 package p0032_longest_valid_parentheses;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /**
  * <a href="https://leetcode.com/problems/longest-valid-parentheses/">32. Longest Valid Parentheses</a>
  * <br><br>
@@ -12,21 +9,38 @@ public class Solution {
 
     public int longestValidParentheses(String s) {
         int longest = 0;
-
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(-1);
+        int left = 0, right = 0;
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                stack.push(i);
+                left++;
             } else {
-                stack.pop();
+                right++;
+            }
 
-                if (stack.isEmpty()) {
-                    stack.push(i);
-                } else {
-                    longest = Math.max(longest, i - stack.peek());
-                }
+            if (left == right) {
+                longest = Math.max(longest, 2 * right);
+            } else if (right > left) {
+                left = 0;
+                right = 0;
+            }
+        }
+
+        left = 0;
+        right = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+
+            if (left == right) {
+                longest = Math.max(longest, 2 * left);
+            } else if (left > right) {
+                left = 0;
+                right = 0;
             }
         }
 
