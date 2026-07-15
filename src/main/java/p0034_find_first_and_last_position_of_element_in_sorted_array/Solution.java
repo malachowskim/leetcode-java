7413f4df855d9ce293b49e6257881c02e20eb925
malachowskim <160923270+malachowskim@users.noreply.github.com>
2026-07-15 15:23:01 +0200

    Task 34 improved solution

--- src/main/java/p0034_find_first_and_last_position_of_element_in_sorted_array/Solution.java
+++ src/main/java/p0034_find_first_and_last_position_of_element_in_sorted_array/Solution.java
@@ -12,19 +12,33 @@ public class Solution {
             return new int[] {-1, -1};
         }
 
-        int lb = -1, rb = -1;
+        int lb = findBound(nums, target, true);
+
+        if (lb == -1) {
+            return new int[] {-1, -1};
+        }
+
+        int rb = findBound(nums, target, false);
+
+        return new int[] {lb, rb};
+    }
+
+    private int findBound(int[] nums, int target, boolean isLeft) {
         int left = 0;
         int right = nums.length - 1;
-        while (left <= right) {
-            if (left == right && nums[left] == target) {
-                lb = left;
-                break;
-            }
+        int bound = -1;
 
+        while (left <= right) {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
-                right = mid;
+                bound = mid;
+
+                if (isLeft) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             } else if (nums[mid] > target) {
                 right = mid - 1;
             } else {
@@ -32,25 +46,6 @@ public class Solution {
             }
         }
 
-        left = 0;
-        right = nums.length - 1;
-        while (left <= right) {
-            if (left == right && nums[left] == target) {
-                rb = left;
-                break;
-            }
-
-            int mid = left + (int) Math.ceil((right - left) / 2.0);
-
-            if (nums[mid] == target) {
-                left = mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return new int[] {lb, rb};
+        return bound;
     }
 }
