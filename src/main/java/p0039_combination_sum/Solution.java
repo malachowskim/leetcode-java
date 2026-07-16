0cc87fe56128b8be80c7e2f4d9768484e703fb8c
malachowskim <160923270+malachowskim@users.noreply.github.com>
2026-07-16 16:38:53 +0200

    Task 39 initial solution

---
+++ src/main/java/p0039_combination_sum/Solution.java
@@ -0,0 +1,53 @@
+package p0039_combination_sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/combination-sum/">39. Combination Sum</a>
+ * <br><br>
+ * Difficulty: Medium
+ */
+public class Solution {
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+
+        int left = 0;
+        int right = candidates.length - 1;
+        while (left < right) {
+            int temp = candidates[left];
+            candidates[left++] = candidates[right];
+            candidates[right--] = temp;
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        recurse(candidates, target, new ArrayList<>(), 0, 0, result);
+
+        return result;
+    }
+
+    private void recurse(int[] candidates, int target, List<Integer> current, int currentSum, int left, List<List<Integer>> result) {
+        if (left >= candidates.length) {
+            if (currentSum == target) {
+                result.add(current);
+            }
+            return;
+        }
+
+        for (int i = left; i < candidates.length; i++) {
+            int chosenNum = candidates[i];
+            currentSum += chosenNum;
+            current.add(chosenNum);
+            int diff = target - currentSum;
+            int newLeft = i;
+            while (newLeft < candidates.length && diff < candidates[newLeft]) {
+                newLeft++;
+            }
+            recurse(candidates, target, new ArrayList<>(current), currentSum, newLeft, result);
+            currentSum -= chosenNum;
+            current.remove((Integer) chosenNum);
+        }
+    }
+}
---
+++ src/test/java/p0039_combination_sum/SolutionTest.java
@@ -0,0 +1,42 @@
+package p0039_combination_sum;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+
+    private final Solution solution = new Solution();
+
+    @Test
+    void test1() {
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+
+        List<List<Integer>> result = solution.combinationSum(candidates, target);
+
+        assertThat(result).containsExactlyInAnyOrder(List.of(3,2,2), List.of(7));
+    }
+
+    @Test
+    void test2() {
+        int[] candidates = {2,3,5};
+        int target = 8;
+
+        List<List<Integer>> result = solution.combinationSum(candidates, target);
+
+        assertThat(result).containsExactlyInAnyOrder(List.of(2,2,2,2), List.of(3,3,2), List.of(5,3));
+    }
+
+    @Test
+    void test3() {
+        int[] candidates = {2};
+        int target = 1;
+
+        List<List<Integer>> result = solution.combinationSum(candidates, target);
+
+        assertThat(result).isEmpty();
+    }
+}
