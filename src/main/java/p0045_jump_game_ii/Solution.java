1cd016c8d96bb4a17746b94c128374dcc402cfae
malachowskim <160923270+malachowskim@users.noreply.github.com>
2026-07-27 11:35:33 +0200

    Task 45 improved solution

--- src/main/java/p0045_jump_game_ii/Solution.java
+++ src/main/java/p0045_jump_game_ii/Solution.java
@@ -1,5 +1,7 @@
 package p0045_jump_game_ii;
 
+import java.util.Arrays;
+
 /**
  * <a href="https://leetcode.com/problems/jump-game-ii/">45. Jump Game II</a>
  * <br><br>
@@ -8,44 +10,19 @@ package p0045_jump_game_ii;
 public class Solution {
 
     public int jump(int[] nums) {
+        int current = 0;
         int[] distances = new int[nums.length];
-        boolean[] bVisitedArr = new boolean[nums.length];
-        for (int i = 0; i < distances.length; i++) {
-            distances[i] = -1;
-            bVisitedArr[i] = false;
-        }
-
+        Arrays.fill(distances, Integer.MAX_VALUE);
         distances[0] = 0;
-        int currentIdx;
-        while ((currentIdx = selectSmallestUnvisited(distances, bVisitedArr)) != -1) {
-            int current = distances[currentIdx];
-            int neighbours = nums[currentIdx];
-            for (int offset = 1; offset <= neighbours; offset++) {
-                int idx = currentIdx + offset;
-                if (idx < distances.length) {
-                    int newCost = current + 1;
-                    if (distances[idx] == -1 || distances[idx] > newCost) {
-                        distances[idx] = newCost;
-                    }
-                }
+
+        while (current < nums.length) {
+            for (int i = 1; i <= nums[current] && current + i < nums.length; i++) {
+                distances[current + i] = Math.min(distances[current + i], distances[current] + 1);
             }
 
-            bVisitedArr[currentIdx] = true;
+            current++;
         }
 
         return distances[distances.length - 1];
-    }
-
-    private int selectSmallestUnvisited(int[] distances, boolean[] bVisited) {
-        int min = Integer.MAX_VALUE;
-        int idx = -1;
-        for (int i = 0; i < distances.length; i++) {
-            if (!bVisited[i] && distances[i] != -1 && distances[i] < min) {
-                min = distances[i];
-                idx = i;
-            }
-        }
-
-        return idx;
     }
 }
