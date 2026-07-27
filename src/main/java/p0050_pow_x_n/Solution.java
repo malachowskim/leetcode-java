5581224d2b9f3bf3e0c9481b5309162154179f45
malachowskim <160923270+malachowskim@users.noreply.github.com>
2026-07-27 18:25:08 +0200

    Task 50 improved solution

--- src/main/java/p0050_pow_x_n/Solution.java
+++ src/main/java/p0050_pow_x_n/Solution.java
@@ -8,6 +8,26 @@ package p0050_pow_x_n;
 public class Solution {
 
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            n = -1 * n;
+            x = 1.0 / x;
+        }
+
+        double result = 1;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                result = result * x;
+                n -= 1;
+            }
+
+            x *= x;
+            n /= 2;
+        }
+
+        return result;
     }
 }
--- src/test/java/p0050_pow_x_n/SolutionTest.java
+++ src/test/java/p0050_pow_x_n/SolutionTest.java
@@ -2,7 +2,7 @@ package p0050_pow_x_n;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class SolutionTest {
 
@@ -16,5 +16,25 @@ class SolutionTest {
         double result = solution.myPow(x, n);
 
         assertThat(result).isEqualTo(1024.00000);
+    }
+
+    @Test
+    void test2() {
+        double x = 2.10000;
+        int n = 3;
+
+        double result = solution.myPow(x, n);
+
+        assertThat(result).isCloseTo(9.26100, within(0.0001));
+    }
+
+    @Test
+    void test3() {
+        double x = 2.00000;
+        int n = -2;
+
+        double result = solution.myPow(x, n);
+
+        assertThat(result).isEqualTo(0.25000);
     }
 }
