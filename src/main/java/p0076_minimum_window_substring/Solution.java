38e056b800e16935a05f743c1154f0ebd9abd1a3
malachowskim <160923270+malachowskim@users.noreply.github.com>
2026-08-21 00:32:33 +0200

    Task 76 improved solution

--- src/main/java/p0076_minimum_window_substring/Solution.java
+++ src/main/java/p0076_minimum_window_substring/Solution.java
@@ -1,7 +1,5 @@
 package p0076_minimum_window_substring;
 
-import java.util.*;
-
 /**
  * <a href="https://leetcode.com/problems/minimum-window-substring/">76. Minimum Window Substring</a>
  * <br><br>
@@ -16,54 +14,43 @@ public class Solution {
             return "";
         }
 
-        int left = 0;
-        int right = 0;
-        Map<Character, Integer> expected = new HashMap<>();
+        int[] targetFreq = new int[128];
         for (char c : t.toCharArray()) {
-            expected.put(c, expected.getOrDefault(c, 0) + 1);
+            targetFreq[c]++;
         }
 
-        Map<Character, Integer> window = new HashMap<>();
-        String minWindow = "";
+        int left = 0;
+        int minLeft = 0;
         int minLen = Integer.MAX_VALUE;
-        while (right < m) {
-            char current = s.charAt(right);
-            if (expected.containsKey(current)) {
-                window.put(current, window.getOrDefault(current, 0) + 1);
+        int charsNeeded = t.length();
 
-                // 1. We might complete the word (maps are equal) - in this case we keep sliding left until the maps are not equal
-                while (areMapsEqual(window, expected)) {
-                    if (right - left + 1 < minLen) {
-                        minWindow = s.substring(left, right + 1);
-                        minLen = right - left + 1;
-                    }
-                    if (expected.containsKey(s.charAt(left))) {
-                        window.put(s.charAt(left), window.getOrDefault(s.charAt(left), 0) - 1);
-                    }
-                    left++;
+        for (int right = 0; right < m; right++) {
+            char rightChar = s.charAt(right);
+
+            if (targetFreq[rightChar] > 0) {
+                charsNeeded--;
+            }
+
+            targetFreq[rightChar]--;
+
+            while (charsNeeded == 0) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    minLeft = left;
                 }
-            }
-            right++;
-        }
 
-        return minWindow;
-    }
+                char leftChar = s.charAt(left);
 
-    private boolean areMapsEqual(Map<Character, Integer> window, Map<Character, Integer> expected) {
-        if (window.size() != expected.size()) {
-            return false;
-        }
+                targetFreq[leftChar]++;
 
-        for (Map.Entry<Character, Integer> entry : window.entrySet()) {
-            if (!expected.containsKey(entry.getKey())) {
-                return false;
-            }
+                if (targetFreq[leftChar] > 0) {
+                    charsNeeded++;
+                }
 
-            if (expected.get(entry.getKey()) > entry.getValue()) {
-                return false;
+                left++;
             }
         }
 
-        return true;
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLen);
     }
 }
