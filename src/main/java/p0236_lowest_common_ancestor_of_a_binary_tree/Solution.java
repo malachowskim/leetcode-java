737f74182ff4abf8a3a89dcc2234993fe0fd490a
malachowskim <160923270+malachowskim@users.noreply.github.com>
2026-08-20 16:35:16 +0200

    Task 236 improved solution

--- src/main/java/p0236_lowest_common_ancestor_of_a_binary_tree/Solution.java
+++ src/main/java/p0236_lowest_common_ancestor_of_a_binary_tree/Solution.java
@@ -1,8 +1,5 @@
 package p0236_lowest_common_ancestor_of_a_binary_tree;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * <a href="https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/">236. Lowest Common Ancestor of a Binary Tree</a>
  * <br><br>
@@ -10,41 +7,17 @@ import java.util.List;
  */
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> pTree = recurse(root, new ArrayList<>(), p.val);
-        List<TreeNode> qTree = recurse(root, new ArrayList<>(), q.val);
-
-        int maxSize = Math.min(pTree.size(), qTree.size());
-        int i;
-        for (i = 1; i < maxSize; i++) {
-            if (pTree.get(i) != qTree.get(i)) {
-                return pTree.get(i - 1);
-            }
+        if (root == null || root == p || root == q) {
+            return root;
         }
 
-        return pTree.get(i - 1);
-    }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-    private List<TreeNode> recurse(TreeNode current, List<TreeNode> history, int target) {
-        history.add(current);
-        if (current.val == target) {
-            return new ArrayList<>(history);
+        if (left != null && right != null) {
+            return root;
         }
 
-        if (current.left != null) {
-            List<TreeNode> list = recurse(current.left, history, target);
-            if (list != null) {
-                return list;
-            }
-        }
-
-        if (current.right != null) {
-            List<TreeNode> list = recurse(current.right, history, target);
-            if (list != null) {
-                return list;
-            }
-        }
-        history.remove(current);
-
-        return null;
+        return left != null ? left : right;
     }
 }
