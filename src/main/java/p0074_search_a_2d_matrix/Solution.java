ecafecd0e06bcf35aa3c9d8c0293e4af1bb91229
malachowskim <160923270+malachowskim@users.noreply.github.com>
2026-09-17 16:42:36 +0200

    Task 74 improved solution

--- src/main/java/p0074_search_a_2d_matrix/Solution.java
+++ src/main/java/p0074_search_a_2d_matrix/Solution.java
@@ -10,39 +10,22 @@ public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
 
-        if (matrix[0][0] == target) {
-            return true;
-        }
+        int left = 0;
+        int right = m * n - 1;
 
-        int left = 0, right = m - 1;
-        while (left < right) {
-            int mid = left + (right - left + 1) / 2;
-            int val = matrix[mid][0];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            if (val == target) {
-                return true;
-            }
-
-            if (val < target) {
-                left = mid;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        int row = left;
-        left = 0;
-        right = n - 1;
-        while (left < right) {
-            int mid = left + (right - left + 1) / 2;
-            int val = matrix[row][mid];
+            int row = mid / n;
+            int col = mid % n;
+            int val = matrix[row][col];
 
             if (val == target) {
                 return true;
             }
 
             if (val < target) {
-                left = mid;
+                left = mid + 1;
             } else {
                 right = mid - 1;
             }
